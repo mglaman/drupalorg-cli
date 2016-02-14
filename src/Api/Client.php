971c8bebb49be866369c88ac99d44cf0618a6f8d
Matt Glaman <nmd.matt@gmail.com>
2016-02-13 21:20:19 -0600

    Create a Request class for the API interaction.

--- src/Api/Client.php
+++ src/Api/Client.php
@@ -10,12 +10,12 @@ class Client {
     /**
      * @var string
      */
-    protected $apiUrl = 'https://www.drupal.org/api-d7/';
+    const API_URL = 'https://www.drupal.org/api-d7/';
 
     public function __construct()
     {
         $this->client = new \GuzzleHttp\Client([
-            'base_uri' => $this->apiUrl,
+            'base_uri' => self::API_URL,
             'cookies' => true,
             'headers' => [
                 'User-Agent' => 'DrupalOrgCli/0.0.1',
@@ -26,13 +26,13 @@ class Client {
     }
 
     /**
-     * @param $endpoint
-     * @param array $options
+     * @param Request $request
      * @return RawResponse
      * @throws \Exception
      */
-    public function request($endpoint, array $options = []) {
-        $res = $this->client->request('GET', $endpoint, $options);
+    public function request(Request $request) {
+        print_r($request->getUrl());
+        $res = $this->client->request('GET', $request->getUrl());
         if ($res->getStatusCode() == 200) {
             return new RawResponse((string) $res->getBody());
         }
@@ -45,24 +45,24 @@ class Client {
      * @return \mglaman\DrupalOrg\RawResponse
      */
     public function getNode($nid) {
-        return $this->request('node/' . $nid);
+        return $this->request(new Request('node/' . $nid));
     }
 
     public function getFile($fid) {
-        return $this->request('file/' . $fid);
+        return $this->request(new Request('file/' . $fid));
     }
 
     public function getPiftJob($jobId) {
-        return $this->request('pift_ci_job/' . $jobId);
+        return $this->request(new Request('pift_ci_job/' . $jobId));
     }
 
     public function getPiftJobs(array $options) {
         $options += [
-            'sort' => 'job_id',
-            'direction' => 'DESC',
+          'sort' => 'job_id',
+          'direction' => 'DESC',
         ];
-        // Guzzle hates me and parameters.
-        return $this->request('https://www.drupal.org/api-d7/pift_ci_job.json?issue_nid=' . $options['issue_nid'] . '&sort='. $options['sort'] . '&direction=' . $options['direction']);
+
+        return $this->request(new Request('pift_ci_job.json', $options));
     }
 
     /**
@@ -70,7 +70,10 @@ class Client {
      * @return \mglaman\DrupalOrg\RawResponse
      */
     public function getProject($machineName) {
-        return $this->request('https://www.drupal.org/api-d7/node.json?field_project_machine_name=' . $machineName);
+        $request = new Request('node.json', [
+            'field_project_machine_name' => $machineName,
+        ]);
+        return $this->request($request);
     }
 
     public function getProjectReleases($projectNid, array $options = []) {
@@ -78,14 +81,12 @@ class Client {
           'field_release_project' => $projectNid,
           'type' => 'project_release',
             // No Dec by default.
-            'field_release_build_type' => 'static',
-            // Current releases only, by default.
-//          'field_release_update_status' => 0,
+          'field_release_build_type' => 'static',
           'sort' => 'nid',
           'direction' => 'DESC',
-            'limit' => 5,
+          'limit' => 5,
         ];
 
-        return $this->request('node.json?' . http_build_query($options));
+        return $this->request(new Request('node.json', $options));
     }
 }
---
+++ src/Api/Request.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace mglaman\DrupalOrg;
+
+class Request
+{
+    const ASC = 'ASC';
+    const DESC = 'DESC';
+
+    protected $baseUri = Client::API_URL;
+    protected $endpoint;
+    protected $options = [];
+
+    /**
+     * Request constructor.
+     *
+     * @param string $endpoint
+     * @param array $options
+     */
+    public function __construct($endpoint = '', array $options = [])
+    {
+        $this->endpoint = $endpoint;
+        $this->options = $options;
+    }
+
+    /**
+     * Get the request URL.
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->baseUri . $this->endpoint . '?' . http_build_query($this->getOptions());
+    }
+
+    /**
+     * Set the request endpoint.
+     *
+     * @param $endpoint
+     * @return $this
+     */
+    public function setEndpoint($endpoint)
+    {
+        $this->endpoint = $endpoint;
+        return $this;
+    }
+
+    /**
+     * Get the request options.
+     *
+     * @return array
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    /**
+     * Set a request option.
+     *
+     * @param $key
+     * @param $value
+     * @return $this
+     */
+    public function setOption($key, $value)
+    {
+        $this->options[$key] = $value;
+        return $this;
+    }
+
+    /**
+     * Set the key to sort key.
+     *
+     * @param $key
+     * @return \mglaman\DrupalOrg\Request
+     */
+    public function setSort($key) {
+        return $this->setOption('sort', $key);
+    }
+
+    /**
+     * Set the direction order.
+     *
+     * @param $direction
+     * @return \mglaman\DrupalOrg\Request
+     */
+    public function setDirection($direction) {
+        return $this->setOption('direction', $direction);
+    }
+}
