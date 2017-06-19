ee17b22f125ccc65a5abfed4f56a40d533542fd3
Matt Glaman <nmd.matt@gmail.com>
2017-06-19 10:33:32 -0500

    Update the release output

--- src/Cli/Command/Project/Releases.php
+++ src/Cli/Command/Project/Releases.php
@@ -27,24 +27,50 @@ class Releases extends Command
     {
         $machineName = $this->stdIn->getArgument('project');
         $project = $this->getProject($machineName)->getList()->offsetGet(0);
-        $releases = $this->client->getProjectReleases($project->nid)
-          ->get('list');
-
-        $this->stdOut->writeln("<comment>" . $project->title . "</comment>");
+        $releases = $this->client->getProjectReleases($project->nid, [
+          'field_release_update_status' => 0,
+        ])->get('list');
         $table = new Table($this->stdOut);
         $table->setHeaders([
+          'Project',
+          'Security coverage',
           'Version',
           'Created',
-            'Description',
-          'ID',
+          'Description',
+          'Link',
         ]);
 
         foreach ($releases as $release) {
+            $releaseDate = (new \DateTime())->setTimestamp($release->created);
+            $now = new \DateTime();
+            $difference = $now->diff($releaseDate);
+            if ($difference->m <= 1) {
+                $format = 'info';
+                $message = "OK";
+            }
+            elseif ($difference->m >= 2 && $difference->m <= 4) {
+                $format = 'comment';
+                $message = "Release soon";
+            }
+            else {
+                $format = 'error';
+                $message = "Release due";
+            }
+
+            if ($release->field_release_version_extra !== NULL) {
+                $securty = '<error>✗ Not covered</error>';
+            }
+            else {
+                $securty = '<info>✓ Covered</info>';
+            }
+
             $table->addRow([
+              $project->title,
+              $securty,
               $release->field_release_version,
-              date('M j, Y', $release->changed),
-                $release->field_release_short_description,
-              $release->nid,
+              "<$format>" . date('M j, Y', $release->created) . " ($message)</$format>",
+              $release->field_release_short_description ?: 'Needs short description',
+              'https://wwww.drupal.org/project/' . $machineName,
             ]);
         }
         $table->render();
